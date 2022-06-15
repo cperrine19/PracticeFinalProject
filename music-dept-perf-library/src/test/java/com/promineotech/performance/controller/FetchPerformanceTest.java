287e03673efe05dfb1fa698aebb5e0a4ff249cfb
Jordan Ladyman <jordan.ladyman@outlook.com>
2022-06-15 11:30:02 -0500

    added files to generate necessary folders when pulled

---
+++ music-dept-perf-library/src/test/java/com/promineotech/performance/controller/FetchPerformanceTest.java
@@ -0,0 +1,14 @@
+package com.promineotech.performance.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FetchPerformanceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
