43fa2a1d526c89f05277454ad467e729846a662d
Chelsea Perrine <perrinechelsea@gmail.com>
2022-06-16 09:31:20 -0500

    practice jun16

---
+++ music-dept-perf-library/target/test-classes/SqlFiles/Music_Dept_Data.sql
@@ -0,0 +1,39 @@
+INSERT INTO composer (first_name, last_name) VALUES ('LUDWIG', 'BEETHOVEN');
+INSERT INTO composer (first_name, last_name) VALUES ('JOHANN', 'BACH');
+INSERT INTO composer (first_name, last_name) VALUES ('WOLFGANG', 'MOZART');
+INSERT INTO composer (first_name, last_name) VALUES ('RICHARD', 'WAGNER');
+INSERT INTO composer (first_name, last_name) VALUES ('CLAUDE', 'DEBUSSY');
+INSERT INTO composer (first_name, last_name) VALUES ('FREDERIC', 'CHOPIN');
+INSERT INTO composer (first_name, last_name) VALUES ('PYOTR', 'TCHAIKOVSKY');
+INSERT INTO composer (first_name, last_name) VALUES ('JOHANNES', 'BRAHMS');
+
+INSERT INTO song_type (performance_type) VALUES ('soprano vocal solo');
+INSERT INTO song_type (performance_type) VALUES ('alto vocal solo');
+INSERT INTO song_type (performance_type) VALUES ('tenor vocal solo');
+INSERT INTO song_type (performance_type) VALUES ('bass vocal solo');
+INSERT INTO song_type (performance_type) VALUES ('vocal duet');
+INSERT INTO song_type (performance_type) VALUES ('vocal trio');
+INSERT INTO song_type (performance_type) VALUES ('vocal quartet');
+INSERT INTO song_type (performance_type) VALUES ('vocal mixed chorus');
+
+INSERT INTO performer (artist_name) VALUES ('JLADYMAN');
+INSERT INTO performer (artist_name) VALUES ('CPERRINE');
+INSERT INTO performer (artist_name) VALUES ('MADEUP SCHOOL CHORUS');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
